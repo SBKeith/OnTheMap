26bf36d58c1774de8a579aa2175fc9f0559ccd8e
Keith Kowalski <keith@swiftlybits.com>
2016-06-04 00:13:51 -0700

    Initial Project Setup

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Keith Kowalski on 6/4/16.
+//  Copyright © 2016 TouchTapApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
